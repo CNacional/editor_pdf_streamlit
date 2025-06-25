416d248f3a0d412ed1b0801abfd3c26c4cc5803b
CNacional <coordenacaonacional10@gmail.com>
2025-06-25 10:04:14 -0300

    Update editor_pdf_streamlit.py

--- editor_pdf_streamlit.py
+++ editor_pdf_streamlit.py
@@ -36,7 +36,7 @@ for option in menu_options:
 if menu is None:
     menu = menu_options[0]
     
-st.subheader(f"Você selecionou: {menu}")
+st.subheader(f"Você selecionou:\n")
 st.subheader(f"{menu}")
 
 uploaded_file = st.file_uploader("📎 Envie um arquivo PDF", type="pdf")
